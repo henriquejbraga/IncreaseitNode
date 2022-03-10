4f13f2e3d546774c954ba07e1cb94df2baa0bf80
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2022-03-10 17:26:35 -0300

    instala nodemoon,implementa get com titulo e versao

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const app = express();
 
+// app.get('/', (_req, res) => {
+//   res.send('Hello world')
+// });
+
+let pjson = require('./package.json');
+
 app.get('/', (_req, res) => {
-  res.send('Hello world')
+  res.send({
+    title: `${pjson.name}`,
+    version: `${pjson.version}`
+  })
 });
+
+console.log(pjson.version);
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000');
